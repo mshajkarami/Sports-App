0c150335634165b30fd66ea337d4fd210f3fed2b
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-27 09:54:51 +0330

    finish sport App

--- app/src/main/java/ir/hajkarami/sportsapp/MainActivity.kt
+++ app/src/main/java/ir/hajkarami/sportsapp/MainActivity.kt
@@ -3,13 +3,13 @@ package ir.hajkarami.sportsapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import ir.hajkarami.sportsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private val sportList: MutableList<Sport> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +17,24 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        setupSportList()
+        setupRecyclerView()
+    }
+
+    private fun setupSportList() {
+        sportList.apply {
+            add(Sport("Football", R.drawable.football))
+            add(Sport("Basketball", R.drawable.basketball))
+            add(Sport("VolleyBall", R.drawable.volley))
+            add(Sport("Tennis", R.drawable.tennis))
+            add(Sport("Ping Pong", R.drawable.ping))
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = MyAdapter(sportList)
+        }
     }
 }
